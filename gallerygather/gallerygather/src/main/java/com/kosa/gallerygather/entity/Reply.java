09bfd89a93f113d88633ae3e645af74cadc05363
Wildturkeyy <109601078+Wildturkeyy@users.noreply.github.com>
2024-08-05 17:44:20 +0900

    modify Entity: reply

--- gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/entity/Reply.java
+++ gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/entity/Reply.java
@@ -14,10 +14,13 @@ public class Reply {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "reply")
     private String reply;
 
+    @Column(name = "reg_date")
     private LocalDateTime regDate;
 
+    @Column(name = "update_date")
     private LocalDateTime updateDate;
 
 

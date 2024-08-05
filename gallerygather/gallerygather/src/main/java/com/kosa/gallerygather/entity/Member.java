062af1820787ca6b17955dab988c8389df497509
Chae-1 <coguddlf1000@naver.com>
2024-08-05 16:39:20 +0900

    add member

--- gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/entity/Member.java
+++ gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/entity/Member.java
@@ -1,18 +1,30 @@
 package com.kosa.gallerygather.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "TBL")
+@Getter
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
     private String password;
 
     private String email;
+
+    private String auth;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime updateDate;
 
 }
 

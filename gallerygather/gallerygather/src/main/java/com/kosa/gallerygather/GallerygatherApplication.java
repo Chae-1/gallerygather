7ab930d20e074fd0ff75163c47170e34e86ef662
Chae-1 <coguddlf1000@naver.com>
2024-08-05 11:52:15 +0900

    project init

---
+++ gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/GallerygatherApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.gallerygather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallerygatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallerygatherApplication.class, args);
+	}
+
+}
---
+++ gallerygather/gallerygather/src/test/java/com/kosa/gallerygather/GallerygatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.gallerygather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallerygatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

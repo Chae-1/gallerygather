c0656e3851fc15d534616deccc42053477b16e05
unknown <parkyoueun@gmail.com>
2024-08-10 20:09:50 +0900

    merge

--- gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/dto/ExhibitionReviewResponseDto.java
+++ gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/dto/ExhibitionReviewResponseDto.java
@@ -1,6 +1,6 @@
 package com.kosa.gallerygather.dto;
 
-<<<<<<< HEAD
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,14 +15,5 @@ public class ExhibitionReviewResponseDto {
     private String title;
     private String content;
     private Double rating;
-=======
-import lombok.Getter;
 
-@Getter
-public class ExhibitionReviewResponseDto {
-    private Long id;
-    private String title;
-    private String content;
-
->>>>>>> 13f3f8230d0f8c8d7e80b3314d3e8bbb11cbaf9e
 }

bfd702913df4a6861d03f3d283560a9c97dc5771
Wildturkeyy <109601078+Wildturkeyy@users.noreply.github.com>
2024-08-12 21:32:49 +0900

    chage review_link

--- gallerygather-front/src/layout/components/ExhibitionReviewCompo.vue
+++ gallerygather-front/src/layout/components/ExhibitionReviewCompo.vue
@@ -8,7 +8,8 @@
             <ul class="review-list">
                 <li class="review-item" v-for="(review, idx) in exhibitReviewList" :key="idx">
                     <div class="review-box">
-                        <router-link :to="{ path: '/reviewdetails/' + review.id }">
+                        <!-- http://localhost:5173/exhibitiondetails/1/reviewdetails/8 -->
+                        <router-link :to="{ path: '/exhibitiondetails/'+ exhibitionId +'/reviewdetails/' + review.id }">
                             <div class="reviewbox-title">
                                 <p>{{ review.title }}</p>
                             </div>

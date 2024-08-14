993132eab277abb12e53a4323c65cb9ff01046d3
Chae-1 <coguddlf1000@naver.com>
2024-08-14 19:09:59 +0900

    fix: remove images

--- gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/dto/ReviewImageRequestDto.java
+++ gallerygather/gallerygather/src/main/java/com/kosa/gallerygather/dto/ReviewImageRequestDto.java
@@ -3,9 +3,11 @@ package com.kosa.gallerygather.dto;
 import com.kosa.gallerygather.entity.ReviewImage;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ReviewImageRequestDto {
     private String path;
     private String originalName;
